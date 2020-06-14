24c355b157818416ec3cf55261e4c0a99a602152
Gertz <GertzNi@intra.ads-root.de>
2020-06-14 20:38:16 +0200

    before merge

--- CGBVRahmenprogramm/A1_Versuch1b/Versuch1b.cpp
+++ CGBVRahmenprogramm/A1_Versuch1b/Versuch1b.cpp
@@ -421,6 +421,8 @@ void RenderScene(void)
 	modelViewMatrix.PushMatrix();
 	m3dQuatToRotationMatrix(rot, rotation);
 	modelViewMatrix.MultMatrix(rot);
+	cameraFrame.TranslateLocal(xPosition,yPosition,zPosition);
+	cameraFrame.RotateLocal(45, 0, 0, 0);
 
 
 	//setze den Shader für das Rendern und übergebe die Model-View-Projection Matrix
